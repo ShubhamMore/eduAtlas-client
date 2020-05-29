54189eb289010b16f180174327e3c632af9ee589
Akash Kulkarni <akashkulkarni796@gmail.com>
2020-05-29 17:01:20 +0530

    akash

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  server: 'http://localhost:5000',
+  server: '',
   razorpayKeyId: 'rzp_test_uyyXNzgLBF3hTU',
   // server_local: 'http://139.59.26.154:5000',
 };
