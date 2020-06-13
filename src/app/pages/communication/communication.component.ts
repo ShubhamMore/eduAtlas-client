b04a80d7b41b1b914a72229025f6cba4bc637bc8
sarvesh <sarvesh10n@gmail.com>
2020-06-13 21:34:27 +0530

    drop down bug fix

--- src/app/pages/communication/communication.component.ts
+++ src/app/pages/communication/communication.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ngx-communication',
-  template: `<nb-layout><nb-layout-column><router-outlet></router-outlet></nb-layout-column></nb-layout>`,
+  template: `<router-outlet></router-outlet>`,
   styles: [],
 })
 export class CommunicationComponent implements OnInit {
