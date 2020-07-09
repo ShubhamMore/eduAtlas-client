fb015a9509970efab34812fedd3797267f052e0b
Shubham More <shubhammore1796@gmail.com>
2020-07-10 00:02:42 +0530

    eduatlas

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   razorpayKeyId: 'rzp_test_EVmv0tdRskXUYb',
-  server: 'http://lms.eduatlas.in',
-  SOCKET_ENDPOINT: 'http://lms.eduatlas.in/eduatlas-chat',
+  server: '',
+  SOCKET_ENDPOINT: '/eduatlas-chat',
 };
