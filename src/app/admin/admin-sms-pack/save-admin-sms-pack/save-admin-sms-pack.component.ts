56d1d122a23ace6e992ec7919ffc4849a26ba8b8
Shubham More <shubhammore1796@gmail.com>
2020-09-02 15:42:01 +0530

    eduatlas

---
+++ src/app/admin/admin-sms-pack/save-admin-sms-pack/save-admin-sms-pack.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-save-admin-sms-pack',
+  templateUrl: './save-admin-sms-pack.component.html',
+  styleUrls: ['./save-admin-sms-pack.component.scss']
+})
+export class SaveAdminSmsPackComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
